6665ad0d38a12205669e8639ee6a0e1f04b42bb4
Mario Reckl <mreckl@uwo.ca>
2016-03-12 02:58:37 -0500

    Clarifai Starter

---
+++ app/src/main/java/com/clarifai/androidstarter/Credentials.java
@@ -0,0 +1,13 @@
+package com.clarifai.androidstarter;
+
+class Credentials {
+  // IMPORTANT NOTE: you should replace the CLIENT_ID and CLIENT_SECRET values below
+  // with your own. You can obtain these at https://developer.clarifai.com/applications -- the
+  // defaults are shared credentials that will stop working once their quota is reached.
+  //
+  // Also, if you're committing your code to a public repo, be careful not to check in your
+  // credentials -- we're including these here so you can get started out of the box, but this is
+  // generally a bad practice.
+    static final String CLIENT_ID = "izh17bw-7bGz2cE55bueFOEe13ujHz6_YmH9R14v";
+    static final String CLIENT_SECRET = "nhsswO3XrL6TFu5T2R261PPsyUv5nlh1tHX6h-Yf";
+}
