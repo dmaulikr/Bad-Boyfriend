93a8afd1f74b6dd5505c8a0a372c2000ae11df88
Mario Reckl <mreckl@uwo.ca>
2016-03-12 03:41:38 -0500

    updated Credentials

--- app/src/main/java/com/clarifai/androidstarter/Credentials.java
+++ app/src/main/java/com/clarifai/androidstarter/Credentials.java
@@ -8,6 +8,6 @@ class Credentials {
   // Also, if you're committing your code to a public repo, be careful not to check in your
   // credentials -- we're including these here so you can get started out of the box, but this is
   // generally a bad practice.
-    static final String CLIENT_ID = "izh17bw-7bGz2cE55bueFOEe13ujHz6_YmH9R14v";
-    static final String CLIENT_SECRET = "nhsswO3XrL6TFu5T2R261PPsyUv5nlh1tHX6h-Yf";
+    static final String CLIENT_ID = "ZBDnp3_CJ-MeQZfMAeErf9vHLIuTutrxdWUN33el";
+    static final String CLIENT_SECRET = "RIPdvyK3Jptc_sNazYIM0qmJplV5fd_xTWvKkQ3v";
 }
